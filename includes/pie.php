54e9edc612e38fbff4aea2a66436f6f067ed818e
Your Name <14658@utcv.edu.mx>
2019-09-16 17:57:49 -0500

    Blog hecho con PHP

---
+++ includes/pie.php
@@ -0,0 +1,11 @@
+
+			<div class="clearfix"></div>
+		</div> <!-- fin contenedor -->
+
+		<!-- PIE DE PÁGINA -->
+		<footer id="pie">
+			<p>Jesus Arturo Morales &copy; 2018</p>
+		</footer>
+
+	</body>
+</html>
